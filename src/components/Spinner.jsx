493d7cf11c81206e15bb30f85229314ca2cab056
Karansgithub <karanshishodia2710@gmail.com>
2023-10-20 09:09:45 +0530

    done

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="loading">
+      <div class="loader"></div>
+    </div>
+  );
+};
+
+export default Spinner;
